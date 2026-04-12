6e177d7f4317283e65357939e4d66921ea69c025
AntonAzer <tonyazer86@gmail.com>
2026-04-12 02:09:40 +0200

    Update chefs.h

--- Classes/chefs.h
+++ Classes/chefs.h
@@ -1,39 +1,32 @@
 #pragma once
-#include "orders.h"
 #include <iostream>
-#include <ostream>
-using namespace std;
-class chefs
-{
 
+class Chefs
+{
 public:
     enum CHFTYPE {
         TYPE_CN,
         TYPE_CS
     };
 
-private :
+private:
     int ID;
     CHFTYPE type;
-    int speed;         
-    int busyTime;    
-    orders* currentOrd; 
-    int finishTime; 
+    int speed;
+    int busyTime;
+    int finishTime;  // Timestep when the chef will finish the current order
 
 public:
-    chefs(int id, CHFTYPE t, int spd);
+    Chefs(int id, CHFTYPE t, int spd);
 
     int getID() const;
     CHFTYPE getType() const;
     int getSpeed() const;
 
-    void assignOrder(orders* ord, int currentTimestep);
+    // --- Added to track when the chef becomes free ---
+    void setFinishTime(int time);
     bool isAvailable(int currentTimestep) const;
+    void makeAvailable(); // Used for cancellation
 
-    friend ostream& operator<<(ostream& os, const chefs* pChf);
-
-
-
-
+    friend std::ostream& operator<<(std::ostream& os, const Chefs* pChf);
 };
-
